c01d01cccf3d24314e3adafd2fbb5947a05f5968
hilbert <hilbertmf@gmail.com>
2022-05-30 00:03:23 -0300

    update leetcode 295 w/ simpler solution

--- facebook/leetcode-295-find-median-from-data-stream.cpp
+++ facebook/leetcode-295-find-median-from-data-stream.cpp
@@ -1,4 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
 // https://leetcode.com/problems/find-median-from-data-stream
+
+// simpler solution using heaps
+// time: O(nlogn)
+// space: O(n)
+
+class MedianFinder {
+public:
+    priority_queue<int> leftMaxHeap;
+    priority_queue<int, vector<int>, greater<int>> rightMinHeap;
+
+    MedianFinder() {
+
+    }
+
+    void addNum(int num) {
+        if(!leftMaxHeap.empty() && num < leftMaxHeap.top()) {
+            leftMaxHeap.push(num);
+            if(leftMaxHeap.size() > rightMinHeap.size() + 1) {
+                rightMinHeap.push(leftMaxHeap.top());
+                leftMaxHeap.pop();
+            }
+        }
+        else {
+            rightMinHeap.push(num);
+            if(rightMinHeap.size() > leftMaxHeap.size() + 1) {
+                leftMaxHeap.push(rightMinHeap.top());
+                rightMinHeap.pop();
+            }
+        }
+    }
+
+    double findMedian() {
+        if((leftMaxHeap.size() + rightMinHeap.size()) % 2 == 1) { // odd
+            if(leftMaxHeap.size() > rightMinHeap.size())
+                return leftMaxHeap.top();
+            else
+                return rightMinHeap.top();
+        }
+        else {
+            return (leftMaxHeap.top() + rightMinHeap.top()) / 2.0;
+        }
+    }
+};
+
+
+// solution using multiset
 // time: O(nlogn)
 // space: O(n)
 class MedianFinder {
