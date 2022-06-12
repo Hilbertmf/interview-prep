3887d1bf7fb63cb7d135df26a4f753599b3d79e9
hilbert <hilbertmf@gmail.com>
2022-06-12 15:05:38 -0300

    improve leetcode 417 solution

--- graphs/leetcode-417-pacific-atlantic-water-flow.cpp
+++ graphs/leetcode-417-pacific-atlantic-water-flow.cpp
@@ -1,64 +1,47 @@
-// dfs solution
+// dfs from borders, stores states in 2d vectors
 // time: O(V*E)
 // space: O(V*E)
-struct pair_hash {
-    inline std::size_t operator()(const std::pair<int,int> & v) const {
-        return v.first*31+v.second;
-    }
-};
-
 class Solution {
 public:
     vector<int> rowDir{1, -1, 0, 0};
     vector<int> colDir{0, 0, 1, -1};
     
-    bool valid(int row, int col, vector<vector<int>>& grid) {
-        return row >= 0 && col >= 0 && row < grid.size() && col < grid[0].size();
+    bool valid(int row, int col, int parentRow, int parentCol, vector<vector<int>>& grid) {
+        return row >= 0 && col >= 0 && row < grid.size() && col < grid[0].size() &&
+        grid[parentRow][parentCol] <= grid[row][col];
+        // we're flowing in reverse so parent <= child
     }
-    
-    pair<bool, bool> traverse(int row, int col, vector<vector<int>>& heights, vector<vector<bool>>& visited,
-    unordered_set<pair<int, int>, pair_hash>& pacificBorder,
-    unordered_set<pair<int, int>, pair_hash>& atlanticBorder, vector<pair<int, int>>& path) {
-        
-        pair<bool, bool> result(false, false);
-        
-        if(pacificBorder.find({row, col}) != pacificBorder.end())
-            result.first = true;
-        if(atlanticBorder.find({row, col}) != pacificBorder.end())
-            result.second = true;
-                
+
+    void traverse(int row, int col, vector<vector<int>>& grid,
+    vector<vector<bool>>& border, vector<vector<bool>>& visited) {
+        if(!border[row][col])
+            border[row][col] = true;
+
         for(int i = 0; i < 4; ++i) {
             int nextRow = row + rowDir[i];
             int nextCol = col + colDir[i];
-            if(!valid(nextRow, nextCol, heights))
+
+            if(!valid(nextRow, nextCol, row, col, grid))
+                continue;
+            if(border[nextRow][nextCol])
                 continue;
 
-            if(!visited[nextRow][nextCol] && heights[nextRow][nextCol] <= heights[row][col]) {
+            if(!visited[nextRow][nextCol]) {
                 visited[nextRow][nextCol] = true;
-                auto p = traverse(nextRow, nextCol, heights, visited, pacificBorder, atlanticBorder, path);
-                result.first |= p.first;
-                result.second |= p.second;
+                traverse(nextRow, nextCol, grid, border, visited);
             }
         }
-
-        if(result.first)
-            pacificBorder.insert({row, col});
-        if(result.second)
-            atlanticBorder.insert({row, col});
-
-        path.push_back({row, col});
-        return result;
     }
-    
+
+
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int rows = heights.size();
         int cols = heights[0].size();
-        vector<vector<int>> cells;
-        vector<vector<bool>> visited(rows, vector<bool>(cols));
-        unordered_set<pair<int, int>, pair_hash> pacificBorder;
-        unordered_set<pair<int, int>, pair_hash> atlanticBorder;
-        pacificBorder.reserve(2 * (rows + cols));
-        atlanticBorder.reserve(2 * (rows + cols));
+        vector<vector<bool>> pacificBorder(rows, vector<bool>(cols));
+        vector<vector<bool>> pacificVisited(rows, vector<bool>(cols));
+
+        vector<vector<bool>> atlanticBorder(rows, vector<bool>(cols));
+        vector<vector<bool>> atlanticVisited(rows, vector<bool>(cols));
 
         // populate pacific
         for(int r = 0; r < rows; ++r) {
@@ -66,7 +49,7 @@ public:
             if(r == 0)
                 colEnd = cols;
             for(int c = 0; c < colEnd; ++c) {
-                pacificBorder.insert({r, c});
+                pacificBorder[r][c] = true;
             }
         }
 
@@ -76,36 +59,39 @@ public:
             if(r == rows - 1)
                 colStart = 0;
             for(int c = colStart; c < cols; ++c) {
-                atlanticBorder.insert({r, c});
+                atlanticBorder[r][c] = true;
             }
         }
 
-        int i;
+        // run dfs from borders
         for(int r = 0; r < rows; ++r) {
-            
-            for(int c = 0; c < cols; ++c) {
-                // contains in atlantic and pacific
-                if(atlanticBorder.find({r, c}) != atlanticBorder.end() &&
-                pacificBorder.find({r, c}) != pacificBorder.end()) {
-                    continue;
-                }
-
-                vector<pair<int, int>> path;
-                visited[r][c] = true;
-                traverse(r, c, heights, visited, pacificBorder, atlanticBorder, path);
-
-                // unmark path
-                for(const auto &p : path) {
-                    visited[p.first][p.second] = false;
-                }
+            int colEnd = 1;
+            if(r == 0)
+                colEnd = cols;
+            for(int c = 0; c < colEnd; ++c) {
+                pacificVisited[r][c] = true;
+                traverse(r, c, heights, pacificBorder, pacificVisited);
+            }
+        }
+        for(int r = 0; r < rows; ++r) {
+            int colStart = cols - 1;
+            if(r == rows - 1)
+                colStart = 0;
+            for(int c = colStart; c < cols; ++c) {
+                atlanticVisited[r][c] = true;
+                traverse(r, c, heights, atlanticBorder, atlanticVisited);
             }
         }
 
-        for(const auto &pacificPair : pacificBorder) {
-            if(atlanticBorder.find(pacificPair) != atlanticBorder.end())
-                cells.push_back(vector<int>{pacificPair.first, pacificPair.second});
+        vector<vector<int>> answer;
+
+        for(int r = 0; r < rows; ++r) {
+            for(int c = 0; c < cols; ++c) {
+                if(pacificBorder[r][c] && atlanticBorder[r][c])
+                    answer.push_back(vector<int>{r, c});
+            }
         }
-        
-        return cells;
+
+        return answer;
     }
 };
